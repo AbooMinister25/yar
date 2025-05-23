39ae41c5ce2665d99e5e938688315739da107634
AbooMinister25 <aboominister@gmail.com>
2025-05-23 17:46:07 -0400

    feat: add config struct

---
+++ crates/site/src/config.rs
@@ -0,0 +1,25 @@
+use std::path::{Path, PathBuf};
+
+use serde::{Deserialize, Serialize};
+
+/// Configuration values for a site.
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Config {
+    pub url: String,
+    pub root: PathBuf,
+    pub output_path: PathBuf,
+    pub development: bool,
+    pub theme: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            url: String::from("http://0.0.0.0:8000/"),
+            root: Path::new("site/").to_owned(),
+            output_path: Path::new("public/").to_owned(),
+            development: false,
+            theme: String::from("Catpuccin-frappe"),
+        }
+    }
+}
--- crates/site/src/lib.rs
+++ crates/site/src/lib.rs
@@ -1,7 +1,24 @@
+mod config;
 mod entry;
 mod sql;
 
-use color_eyre::Result;
+use config::Config;
+use entry::Entry;
+use rusqlite::Connection;
 
 /// A site to be built.
-pub struct Site {}
+pub struct Site {
+    conn: Connection,
+    config: Config,
+    entries: Vec<Entry>,
+}
+
+impl Site {
+    pub fn new(conn: Connection, config: Config) -> Self {
+        Self {
+            conn,
+            config,
+            entries: Vec::new(),
+        }
+    }
+}
