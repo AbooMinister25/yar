5fcf36eafbf8da61950d522ae7f424af13d97d58
AbooMinister25 <aboominister@gmail.com>
2025-05-23 15:25:29 -0400

    feat: add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use color_eyre::Result;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Build the site.
+    Build {
+        /// Run a clean build. Deletes database.
+        #[arg(long)]
+        clean: bool,
+    },
+}
+
+fn main() -> Result<()> {
+    let arguments = Args::parse();
+
+    Ok(())
 }
